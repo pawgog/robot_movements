3d7f2e3ada3057005ca96f3e2ac76c8df3e0fabc
pawgog <pgogola10@gmail.com>
2022-12-16 16:28:26 +0100

    Add box shadow.

--- src/component/ControlPanel/RobotMovement.styled.ts
+++ src/component/ControlPanel/RobotMovement.styled.ts
@@ -7,6 +7,8 @@ export const MovementsBoard = styled.div`
   justify-content: center;
   padding: 0.5rem;
   background: ${colors.white};
+  box-shadow: 0px 0px 4px -1px rgb(0 0 0 / 20%),
+    0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
 
   & > div {
     margin: 0.5rem;
